5c68798a12f9f8ea224fb844c5f7bf892885c0ad
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-11-20 10:25:26 +0900

    Update

--- lang_lisp_python/main.py
+++ lang_lisp_python/main.py
@@ -1,2 +1,14 @@
+def read_expr() -> str:
+    return input()
+
+
 def main():
     print("main")
+    while True:
+         print("lisp > ", end="")
+         expr = read_expr()
+         print(expr)
+
+
+if __name__ == "__main__":
+    main()
