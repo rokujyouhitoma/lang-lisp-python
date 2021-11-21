936c57b878064ce086e9c599c264408eb6375c39
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-11-21 11:39:14 +0900

    Update

--- lang_lisp_python/main.py
+++ lang_lisp_python/main.py
@@ -1,9 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class Env:
+    pass
+
+
+def default_env():
+    data = {}
+    return Env(data, None)
+
+
 def read_expr() -> str:
     return input()
 
 
 def main():
     print("main")
+    env = default_env();
     while True:
          print("lisp > ", end="")
          expr = read_expr()
