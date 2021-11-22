9114eb8939246019e84152bd6d5d4f9ffe4a9eb4
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-11-22 19:12:15 +0900

    Update

--- lang_lisp_python/main.py
+++ lang_lisp_python/main.py
@@ -1,8 +1,26 @@
+import typing
 from dataclasses import dataclass
 
 @dataclass
+class Err:
+    reason: str
+
+
+@dataclass
+class Result:
+    value: typing.Any
+    err: Err
+
+
+class Expression:
+    def to_string(self) -> str:
+        pass
+
+
+@dataclass
 class Env:
-    pass
+    data: typing.Dict[str, Expression]
+    outer: typing.Optional[Expression]
 
 
 def default_env():
@@ -10,17 +28,65 @@ def default_env():
     return Env(data, None)
 
 
+def tokenize(expr: str) -> typing.Sequence[str]:
+    return [c for c in expr.replace("(", " ( ").replace(")", " ) ").split(" ") if c != ""]
+
+
+def read_seq(tokens: typing.Sequence[str]) -> Result:
+    res = []
+    xs = tokens
+    while xs:
+        if len(tokens) == 0:
+            return Result(None, Err("could not find closing `)`"))
+        next_token, rest = xs[0], xs[1:]
+        if next_token == ")":
+            return Ok((List(res), rest))
+        (exp, new_xs) = parse(xs).value
+        res.append(exp)
+        xs = new_xs
+
+
+def parse_atom(token: str):
+    if token == "true":
+        return Bool(True)
+    elif token == "false":
+        return Bool(False)
+    try:
+        potential_float = int(token)
+        return Number(potential_float)
+    except ValueError:
+        return Symbol(token)
+
+
+def parse(tokens: typing.Sequence[str]) -> Result:
+    if len(tokens) == 0:
+        return Result(None, Err("could not get token"))
+    token, rest = tokens[0], tokens[1:]
+    if token == "(":
+        return read_seq(rest)
+    elif token == ")":
+        return Result(None, Err("unexpected )"))
+    else:
+        return Result((parse_atom(token), rest), None)
+
+
+def parse_eval(expr: str, env: Env) -> Result:
+    (parsed_exp, _) = parse(tokenize(expr)).value;
+    evaled_exp = eval(parsed_exp, env);
+    return Result(evaled_exp, None)
+
+
 def read_expr() -> str:
     return input()
 
 
 def main():
-    print("main")
     env = default_env();
     while True:
          print("lisp > ", end="")
          expr = read_expr()
          print(expr)
+         result = parse_eval(expr, env).value
 
 
 if __name__ == "__main__":
