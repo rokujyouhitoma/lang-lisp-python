3a1f1463e2269724128828ab830b22c649c97a23
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-12-01 19:17:54 +0900

    Update

--- lang_lisp_python/main.py
+++ lang_lisp_python/main.py
@@ -2,6 +2,7 @@ import typing
 from dataclasses import dataclass
 from enum import Enum
 
+
 @dataclass
 class Err:
     reason: str
@@ -92,6 +93,18 @@ def read_seq(tokens: typing.Sequence[str]) -> Result:
         xs = new_xs
 
 
+def env_get(k: str, env: Env):
+    exp = env.data.get(k.value)
+    if exp:
+        return exp
+    else:
+        outer_env = env.outer
+        if outer_env:
+            return env_get(k, outer_env)
+        else:
+            return None
+
+
 def parse_atom(token: str):
     if token == "true":
         return Bool(True)
@@ -116,6 +129,103 @@ def parse(tokens: typing.Sequence[str]) -> Result:
         return Result((parse_atom(token), rest), None)
 
 
+def eval_forms(arg_forms: Expression, env: Env):
+    return Ok([eval(x, env) for x in arg_forms])
+
+
+def parse_list_of_symbol_strings(form: Expression):
+    return form
+
+
+def env_for_lambda(params: Expression, arg_forms: Expression, outer_env: Env):
+    ks = parse_list_of_symbol_strings(params)
+    if len(ks.value) != len(arg_forms):
+        return Err("expected {} arguments, got {}".format(len(ks.value), len(arg_forms)))
+    vs = eval_forms(arg_forms, outer_env)
+    data = {}
+    for k, v in zip(ks, vs):
+        data[k] = v
+    return Ok(Env(data, outer_env))
+
+
+def eval_if_args(arg_forms: typing.Sequence[Expression], env: Env):
+    pass
+
+
+def eval_def_args(arg_forms: typing.Sequence[Expression], env: Env):
+    if len(arg_forms) == 0:
+        return Result(None, Err("expected first form"))
+    first_form = arg_forms[0]
+    if isinstance(first_form, Symbol):
+        first_str = Ok(first_form)
+    else:
+        return Err("expected first form to be a symbol")
+    if len(arg_forms) > 1:
+        second_form = arg_forms[1]
+    else:
+        return Err("expected second form")
+    if len(arg_forms) > 2:
+        return Err("def can only have two forms")
+    second_eval = eval(second_form, env)
+    env.data[first_str.value.value] = second_eval;
+    return Ok(first_form)
+
+
+def eval_lambda_args(arg_forms: typing.Sequence[Expression], env: Env) -> Result:
+    if len(arg_forms) == 0:
+        return Result(None, Err("expected args form"))
+    params_exp = arg_forms[0]
+    if len(arg_forms) > 1:
+        body_exp = arg_forms[1]
+    else:
+        return Err("expected second form")
+    if len(arg_forms) > 2:
+        return Err("fn definition can only have two forms")
+    return Ok(Lambda(body_exp, params_exp))
+
+
+def eval_built_in_form(exp: Expression, arg_forms: typing.Sequence[Expression], env: Env) -> typing.Optional[Result]:
+    if isinstance(exp, Symbol):
+        if exp.value == "if":
+            return eval_if_args(arg_forms, env)
+        elif exp.value == "def":
+            return eval_def_args(arg_forms, env)
+        elif exp.value == "fn":
+            return eval_lambda_args(arg_forms, env)
+        else:
+            return None
+    else:
+        return None
+
+
+def eval(exp: Expression, env: Env) -> Result:
+    if isinstance(exp, Symbol):
+        return env_get(exp, env)
+    elif isinstance(exp, Bool):
+        return exp
+    elif isinstance(exp, Number):
+        return exp
+    elif isinstance(exp, List):
+        if len(exp.value) == 0:
+            return Result(None, Err("expected a non-empty list"))
+        first_form = exp.value[0]
+        arg_forms = exp.value[1:]
+        res = eval_built_in_form(first_form, arg_forms, env)
+        if res:
+            return res
+        else:
+            first_eval = eval(first_form, env).value
+            if isinstance(first_eval, Func):
+                def f(v):
+                    return v
+                return f(eval_forms(arg_forms, env))
+            elif isinstance(first_eval, Lambda):
+                new_env = env_for_lambda(first_eval.params_exp, arg_forms, env)
+                return eval(first_eval.body_exp, new_env)
+            else:
+                return Err("first form must be a function")
+
+
 def parse_eval(expr: str, env: Env) -> Result:
     (parsed_exp, _) = parse(tokenize(expr)).value;
     evaled_exp = eval(parsed_exp, env);
@@ -128,11 +238,19 @@ def read_expr() -> str:
 
 def main():
     env = default_env();
-    while True:
-         print("lisp > ", end="")
-         expr = read_expr()
-         print(expr)
-         result = parse_eval(expr, env).value
+    # while True:
+    #     print("lisp > ", end="")
+    #     expr = read_expr()
+    #     result = parse_eval(expr, env).value
+    #     print(result)
+    #result = parse_eval("(1 2)", env).value
+    #result = parse_eval("(def f 1)", env).value
+    #result = parse_eval("f", env).value
+    q = "(def add-one (fn (a) (+ 1 a)))"
+    result = parse_eval(q, env).value
+    result = parse_eval("(add-one 1)", env).value
+    print(result)
+    print(env)
 
 
 if __name__ == "__main__":
